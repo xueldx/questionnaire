ffdb6d6e86cd3fc25217d5e6500ed318c07241f1
xueldx <13790756845@163.com>
2025-05-03 17:06:03 +0800

    feat: BE - 新增问卷模块实体类

--- packages/questionnaire-be/src/question/entities/question.entity.ts
+++ packages/questionnaire-be/src/question/entities/question.entity.ts
@@ -1,1 +1,26 @@
-export class Question {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+// 问卷模块实体类
+@Entity()
+export class Question {
+  @PrimaryGeneratedColumn()
+  id: number; // 主键ID
+
+  @Column()
+  title: string; // 标题
+
+  @Column()
+  answerCount: string; // 填写次数
+
+  @Column()
+  createTime: string; // 创建时间
+
+  @Column()
+  isPublished: boolean; // 是否发布
+
+  @Column()
+  isStar: boolean; // 是否收藏
+
+  @Column()
+  isDeleted: boolean; // 是否删除
+}
