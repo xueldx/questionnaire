e93da0d21173af1a4fc4cfc58d6cf63f62185c15
xueldx <13790756845@163.com>
2025-11-04 16:25:15 +0800

    feat: BE - 后端接入mongodb

--- packages/questionnaire-be/src/service/question/question.module.ts
+++ packages/questionnaire-be/src/service/question/question.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { QuestionService } from '@/service/question/question.service';
 import { QuestionController } from '@/service/question/question.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Question from '@/common/entities/question.entity';
 import UserFavorite from '@/common/entities/user-favorite.entity';
 import User from '@/common/entities/user.entity';
+import { Schema } from 'mongoose';
+
+const QuestionSchema = new Schema({
+  title: String,
+  createdAt: Date,
+});
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Question, UserFavorite, User])],
+  imports: [
+    TypeOrmModule.forFeature([Question, UserFavorite, User]),
+    MongooseModule.forFeature([{ name: 'Question', schema: QuestionSchema }]),
+  ],
   controllers: [QuestionController],
   providers: [QuestionService],
 })
